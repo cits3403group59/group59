56daea424c56ccd6f36541477a52f09b3db943e3
Sharan Prabhu <sharan@Sharans-MacBook-Air.local>
2025-04-30 00:50:35 +0800

    Added login, signup and upload data page with updated links on python and html files

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,23 @@ from app import app
 # Route for the introductory page
 @app.route('/')
 def introductory():
-    return render_template('introductory.html') 
+    return render_template('introductory.html')
+
+# Route for the login page
+@app.route('/login')
+def login():
+    return render_template('login-page.html')
+
+# Route for the account creation page
+@app.route('/signup')
+def signup():
+    return render_template('signup-page.html')
+
+
+# Route for the upload data page
+@app.route('/upload-data')
+def upload_data():
+    return render_template('upload-data-page.html')
 
 # Run the application
 if __name__ == '__main__':
