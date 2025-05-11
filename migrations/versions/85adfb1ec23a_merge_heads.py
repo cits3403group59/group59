b908f80d80655df8ae5098ad38ec0cf00a30775e
OliviaFitzgerald <ofitz2401@gmail.com>
2025-05-11 16:02:06 +0800

    Merged db heads and removed duplicate friendships and friend requests table from migration folders

---
+++ migrations/versions/85adfb1ec23a_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 85adfb1ec23a
+Revises: c5029594f206, fcd004af49fa
+Create Date: 2025-05-11 15:55:52.605522
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '85adfb1ec23a'
+down_revision = ('c5029594f206', 'fcd004af49fa')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
